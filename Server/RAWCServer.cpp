3ec3875efa67e078f64d919ac5cef9d9dba818c9
Connor Worley <connorbworley@gmail.com>
2012-04-09 14:24:51 -0700

    Added server files

---
+++ Server/RAWCServer.cpp
@@ -0,0 +1,129 @@
+#include "RAWCServer.h"
+
+RAWCServer::RAWCServer(unsigned int port)
+{
+    count = 0;
+    fd = 0;
+    int sock_fd;
+    struct sockaddr_in server;
+
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock_fd == -1)
+    {
+        printf("Error: couldn't create socket on port %d!\n", port);
+    }
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    int status = bind(sock_fd, (sockaddr*)&server, sizeof(server));
+    if(status == -1)
+    {
+        printf("Error: couldn't bind socket on port %d\n", port);
+    }
+
+    status = listen(sock_fd, 10);
+    if(status == -1)
+    {
+        printf("Error: couldn't listen to socket on port %d\n", port);
+    }
+
+    fd = sock_fd;
+    fcntl(fd, F_SETFL, O_NONBLOCK);
+}
+
+RAWCServer::~RAWCServer()
+{
+
+}
+
+void* RAWCServer::handleConnection(void* arg)
+{
+	printf("Handler thread started\n");
+	int client = ((threadArgs*)arg)->client;
+	Stack* stack = ((threadArgs*)arg)->stack;
+
+	while(true)
+	{
+		stack->lock();
+		while(!stack->queue()->empty())
+		{
+			char* s = stack->queue()->front();
+			stack->queue()->pop();
+			send(client, s, strlen(s), 0);
+		}
+		stack->unlock();
+		usleep(100000);
+	}
+
+	delete (threadArgs*)arg;
+	return NULL;
+}
+
+void RAWCServer::print(char* format, ...)
+{
+	char buffer[256];
+	va_list args;
+	va_start(args, format);
+	vsnprintf(buffer, 256, format, args);
+	for(std::vector<RAWCServer::Stack*>::iterator it = stacks.begin(); it != stacks.end(); it++)
+	{
+		(*it)->lock();
+		(*it)->queue()->push(buffer);
+		(*it)->unlock();
+	}
+	va_end(args);
+}
+
+void RAWCServer::handle()
+{
+    struct sockaddr client;
+    int addrlen = sizeof(client);
+    int result = accept(fd, &client, (socklen_t*)&addrlen);
+    if(result == -1 && errno == EWOULDBLOCK)
+    {
+    	return;
+    }
+
+    printf("Connection!\n");
+
+    threadArgs* args = new threadArgs();
+
+    Stack* s = new Stack();
+    stacks.push_back(s);
+
+    args->client = result;
+    args->stack = s;
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, &handleConnection, args);
+}
+
+RAWCServer::Stack::Stack()
+{
+	mutex = new pthread_mutex_t;
+	m_queue = new std::queue<char*>();
+	pthread_mutex_init(mutex, NULL);
+}
+
+RAWCServer::Stack::~Stack()
+{
+	pthread_mutex_destroy(mutex);
+	delete mutex;
+}
+
+std::queue<char*>* RAWCServer::Stack::queue()
+{
+	return m_queue;
+}
+
+void RAWCServer::Stack::lock()
+{
+	pthread_mutex_lock(mutex);
+}
+
+void RAWCServer::Stack::unlock()
+{
+	pthread_mutex_unlock(mutex);
+}
+
---
+++ Server/RAWCServer.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <queue>
+#include <vector>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+class RAWCServer {
+public:
+    RAWCServer(unsigned int port);
+    ~RAWCServer();
+    void handle();
+
+    void print(char* format, ...);
+
+    class Stack
+    {
+        public:
+            Stack();
+            ~Stack();
+            void lock();
+            void unlock();
+            std::queue<char*>* queue();
+        private:
+            std::queue<char*>* m_queue;
+            pthread_mutex_t* mutex;
+    };
+private:
+    int count;
+    int fd;
+    static void* handleConnection(void* arg);
+    std::vector<RAWCServer::Stack*> stacks;
+};
+
+typedef struct threadArgs {
+	int client;
+	RAWCServer::Stack* stack;
+} threadArgs;
